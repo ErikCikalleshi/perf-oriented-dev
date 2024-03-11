e2e493321b8b101a7fc9ce3ca120e9d5a935964c
LV mit LCC <cb761222@login.lcc3.uibk.ac.at>
2024-03-11 21:30:42 +0100

    Fix script

--- small_samples/build/cmake_install.cmake
+++ small_samples/build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/erikck/perf-oriented-dev/small_samples
+# Install script for directory: /home/cb76/cb761222/perf-oriented-dev/small_samples
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -29,7 +29,7 @@ endif()
 
 # Install shared libraries without execute permission?
 if(NOT DEFINED CMAKE_INSTALL_SO_NO_EXE)
-  set(CMAKE_INSTALL_SO_NO_EXE "1")
+  set(CMAKE_INSTALL_SO_NO_EXE "0")
 endif()
 
 # Is this installation the result of a crosscompile?
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/erikck/perf-oriented-dev/small_samples/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/cb76/cb761222/perf-oriented-dev/small_samples/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
