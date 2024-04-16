17effa92003302ba373b3ed4aacecf7383e703a9
Cikalleshi Erik <erik.ck@outlook.it>
2024-04-16 09:54:26 +0200

    update

--- sol/4/a/massif.out.s
+++ sol/4/a/massif.out.s
@@ -12,90 +12,77 @@ heap_tree=empty
 #-----------
 snapshot=1
 #-----------
-time=202300
-mem_heap_B=157058
-mem_heap_extra_B=22
+time=203007
+mem_heap_B=488
+mem_heap_extra_B=16
 mem_stacks_B=0
 heap_tree=empty
 #-----------
 snapshot=2
 #-----------
-time=205821
-mem_heap_B=157546
-mem_heap_extra_B=38
+time=203433
+mem_heap_B=488
+mem_heap_extra_B=16
 mem_stacks_B=0
-heap_tree=empty
+heap_tree=detailed
+n1: 488 (heap allocation functions) malloc/new/new[], --alloc-fns, etc.
+ n1: 488 0x524773E: __fopen_internal (in /usr/lib64/libc-2.28.so)
+  n0: 488 0x4007D7: main (in /home/cb76/cb761222/perf-oriented-dev/larger_samples/npb_bt/build/npb_bt_s)
 #-----------
 snapshot=3
 #-----------
-time=206247
-mem_heap_B=157546
-mem_heap_extra_B=38
+time=203433
+mem_heap_B=0
+mem_heap_extra_B=0
 mem_stacks_B=0
-heap_tree=detailed
-n2: 157546 (heap allocation functions) malloc/new/new[], --alloc-fns, etc.
- n1: 157058 0x52F1D22: monstartup (in /usr/lib64/libc-2.28.so)
-  n1: 157058 0x400F70: __gmon_start__ (in /home/cb76/cb761222/perf-oriented-dev/larger_samples/npb_bt/build/npb_bt_s)
-   n1: 157058 0x4C35A15: ??? (in /usr/libexec/valgrind/vgpreload_massif-amd64-linux.so)
-    n1: 157058 0x40004EF: ??? (in /usr/lib64/ld-2.28.so)
-     n1: 157058 0x4008ED9: call_init.part.0 (dl-init.c:58)
-      n1: 157058 0x400909D: call_init (dl-init.c:30)
-       n1: 157058 0x400909D: _dl_init (dl-init.c:86)
-        n0: 157058 0x401DCB9: ??? (in /usr/lib64/ld-2.28.so)
- n0: 488 in 1 place, below massif's threshold (1.00%)
+heap_tree=empty
 #-----------
 snapshot=4
 #-----------
-time=206247
-mem_heap_B=157058
-mem_heap_extra_B=22
+time=204457
+mem_heap_B=8192
+mem_heap_extra_B=8
 mem_stacks_B=0
 heap_tree=empty
 #-----------
 snapshot=5
 #-----------
-time=207230
-mem_heap_B=165250
-mem_heap_extra_B=30
+time=204722
+mem_heap_B=8680
+mem_heap_extra_B=24
 mem_stacks_B=0
 heap_tree=empty
 #-----------
 snapshot=6
 #-----------
-time=207495
-mem_heap_B=165738
-mem_heap_extra_B=46
-mem_stacks_B=0
-heap_tree=empty
-#-----------
-snapshot=7
-#-----------
-time=207942
-mem_heap_B=165738
-mem_heap_extra_B=46
+time=205148
+mem_heap_B=8680
+mem_heap_extra_B=24
 mem_stacks_B=0
 heap_tree=peak
-n3: 165738 (heap allocation functions) malloc/new/new[], --alloc-fns, etc.
- n1: 157058 0x52F1D22: monstartup (in /usr/lib64/libc-2.28.so)
-  n1: 157058 0x400F70: __gmon_start__ (in /home/cb76/cb761222/perf-oriented-dev/larger_samples/npb_bt/build/npb_bt_s)
-   n1: 157058 0x4C35A15: ??? (in /usr/libexec/valgrind/vgpreload_massif-amd64-linux.so)
-    n1: 157058 0x40004EF: ??? (in /usr/lib64/ld-2.28.so)
-     n1: 157058 0x4008ED9: call_init.part.0 (dl-init.c:58)
-      n1: 157058 0x400909D: call_init (dl-init.c:30)
-       n1: 157058 0x400909D: _dl_init (dl-init.c:86)
-        n0: 157058 0x401DCB9: ??? (in /usr/lib64/ld-2.28.so)
+n2: 8680 (heap allocation functions) malloc/new/new[], --alloc-fns, etc.
  n1: 8192 0x5246D3F: _IO_file_doallocate (in /usr/lib64/libc-2.28.so)
   n1: 8192 0x5254CAF: _IO_doallocbuf (in /usr/lib64/libc-2.28.so)
    n1: 8192 0x5253F17: _IO_file_overflow@@GLIBC_2.2.5 (in /usr/lib64/libc-2.28.so)
     n1: 8192 0x52530BE: _IO_file_xsputn@@GLIBC_2.2.5 (in /usr/lib64/libc-2.28.so)
      n1: 8192 0x5249182: puts (in /usr/lib64/libc-2.28.so)
-      n0: 8192 0x4009C4: main (in /home/cb76/cb761222/perf-oriented-dev/larger_samples/npb_bt/build/npb_bt_s)
- n0: 488 in 1 place, below massif's threshold (1.00%)
+      n0: 8192 0x400894: main (in /home/cb76/cb761222/perf-oriented-dev/larger_samples/npb_bt/build/npb_bt_s)
+ n2: 488 0x524773E: __fopen_internal (in /usr/lib64/libc-2.28.so)
+  n0: 488 0x4008A3: main (in /home/cb76/cb761222/perf-oriented-dev/larger_samples/npb_bt/build/npb_bt_s)
+  n0: 0 in 1 place, below massif's threshold (1.00%)
+#-----------
+snapshot=7
+#-----------
+time=205148
+mem_heap_B=8192
+mem_heap_extra_B=8
+mem_stacks_B=0
+heap_tree=empty
 #-----------
 snapshot=8
 #-----------
-time=207942
-mem_heap_B=165250
-mem_heap_extra_B=30
+time=536113830
+mem_heap_B=0
+mem_heap_extra_B=0
 mem_stacks_B=0
 heap_tree=empty
