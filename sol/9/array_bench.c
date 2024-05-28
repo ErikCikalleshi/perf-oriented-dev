6544192f5a0b488df5247f15a21423990af855ad
ErikCik <erik.ck@outlook.it>
2024-05-28 15:04:56 +0200

    big uf

--- sol/9/array_bench.c
+++ sol/9/array_bench.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <string.h> 
-
+#include <string.h>
 #define TIME_LIMIT 4 // time limit for the benchmark in seconds
 
 volatile int read_value;
@@ -28,37 +27,36 @@ void deletion(int index, int* array, int size) {
     }
 }*/
 
-void read(int index, void* array, int element_size) {
-    void* element = malloc(element_size);
-    memcpy(element, (char*)array + index * element_size, element_size);
-    free(element);
+
+void read(int index, void* array, size_t element_size) {
+    void* element = (char*)array + index * element_size;
+    // use element
 }
 
-void write(int index, int value, void* array, int element_size) {
-    memcpy((char*)array + index * element_size, &value, element_size);
+void write(int index, int value, void* array, size_t element_size) {
+    void* element = (char*)array + index * element_size;
+    memcpy(element, &value, element_size);
 }
 
-void insertion(int index, int value, void* array, int size, int element_size) {
-    for (int i = size - 1; i > index; i--) {
-        memcpy((char*)array + i * element_size, (char*)array + (i - 1) * element_size, element_size);
-    }
-    memcpy((char*)array + index * element_size, &value, element_size);
+void insertion(int index, int value, void* array, size_t size, size_t element_size) {
+    void* element = (char*)array + index * element_size;
+    memmove((char*)element + element_size, element, (size - index - 1) * element_size);
+    memcpy(element, &value, element_size);
 }
 
-void deletion(int index, void* array, int size, int element_size) {
-    for (int i = index; i < size - 1; i++) {
-        memcpy((char*)array + i * element_size, (char*)array + (i + 1) * element_size, element_size);
-    }
+void deletion(int index, void* array, size_t size, size_t element_size) {
+    void* element = (char*)array + index * element_size;
+    memmove(element, (char*)element + element_size, (size - index - 1) * element_size);
 }
 
-void benchmark(int ins_del_ratio, int read_write_ratio, int element_size, int num_elements) {
+void benchmark(int ins_del_ratio, int read_write_ratio, size_t element_size, size_t num_elements) {
     long long int operations = 0;
     time_t start = time(NULL);
 
     void* array = malloc((num_elements + 1) * element_size); // allocate array dynamically
 
     while (1) {
-        for (int i = 0; i < num_elements; i++) {
+        for (size_t i = 0; i < num_elements; i++) {
             if (time(NULL) - start >= TIME_LIMIT) {
                 time_t end = time(NULL);
                 printf("%lld,", operations);
@@ -84,15 +82,21 @@ void benchmark(int ins_del_ratio, int read_write_ratio, int element_size, int nu
 }
 
 int main(int argc, char *argv[]) {
-    if (argc != 5) {
-        printf("Usage: %s ins_del_ratio read_write_ratio element_size num_elements\n", argv[0]);
-        return 1;
-    }
+    // if (argc != 5) {
+    //     printf("Usage: %s ins_del_ratio read_write_ratio element_size num_elements\n", argv[0]);
+    //     return 1;
+    // }
  
-    int ins_del_ratio = atoi(argv[1]);
-    int read_write_ratio = atoi(argv[2]);
-    int element_size = atoi(argv[3]);
-    int num_elements = atoi(argv[4]);
+    // int ins_del_ratio = atoi(argv[1]);
+    // int read_write_ratio = atoi(argv[2]);
+    // int element_size = atoi(argv[3]);
+    // int num_elements = atoi(argv[4]);
+    // 0 100 512 10000000
+
+    int ins_del_ratio = 0;
+    int read_write_ratio = 100;
+    size_t element_size = 512;
+    size_t num_elements = 10000000;
 
     benchmark(ins_del_ratio, read_write_ratio, element_size, num_elements);
     return 0;
