4eb85e76d5c2e35f04fefc4bcfd4112c4334a760
Cikalleshi Erik <erik.ck@outlook.it>
2024-06-15 00:01:18 +0200

    update

---
+++ lua/script.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# SBATCH directives
+#SBATCH --partition=lva
+#SBATCH --job-name benchmark
+#SBATCH --output benchmark.log
+#SBATCH --ntasks=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --exclusive
+
+perf record /home/cb76/cb761222/perf-oriented-dev/lua/interpreter/lua ../fib.lua 
+# perf annotate --stdio luaD_precall
+# perf annotate --stdio luaV_execute
+perf report
